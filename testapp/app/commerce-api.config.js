8f4ca84ec643d07e183a84c25c0e1c3ad149dcf1
Dereniowski <daniel.dereniowski@accenture.com>
2021-09-10 15:03:32 +0200

    Init commit

---
+++ testapp/app/commerce-api.config.js
@@ -0,0 +1,9 @@
+export const commerceAPIConfig = {
+    proxyPath: `/mobify/proxy/api`,
+    parameters: {
+        clientId: '361b931a-2ad5-417a-bf16-cee1a70e852f',
+        organizationId: 'f_ecom_zzrl_028',
+        shortCode: 'kv7kzm78',
+        siteId: 'SalesforcePwaKit'
+    }
+}
